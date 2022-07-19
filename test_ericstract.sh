ee54ce74f1ac81dd2afe9cc2f3abb178b34a4d22
Laurent Ghigonis <ooookiwi@gmail.com>
2022-07-19 19:47:57 +0200

    fix indent in test

--- test_ericstract.sh
+++ test_ericstract.sh
@@ -11,8 +11,8 @@ set -e
 do_test() {
 	expect_sources=$1
 	expect_records=$2
-    expect_unknown=$3
-    expect_binwalk=$4
+	expect_unknown=$3
+	expect_binwalk=$4
 	expect_extracted=$5
 	up_dir=$6
 
@@ -24,12 +24,12 @@ do_test() {
 	tail -n 13 $LOG > $LOG.sum
 	cat $LOG.sum
 
-	[ $(grep "source upgrade files" $LOG.sum |cut -d: -f2) == $expect_sources ]		|| err "bad number of source files"
-	[ $(grep "total number of records" $LOG.sum |cut -d: -f2) == $expect_records ]	|| err "bad number of records found"
-	[ $(grep "unknown records" $LOG.sum |cut -d: -f2) == $expect_unknown ]	    	|| err "bad number of unknown records"
-	[ $(grep "records use binwalk" $LOG.sum |cut -d: -f2) == $expect_binwalk ]	    || err "bad number of binwalk uses"
-	[ $(grep "warnings" $LOG.sum |cut -d: -f2) == 0 ]								|| err "warnings found"
-	[ $(grep "extracted files" $LOG.sum |cut -d: -f2) == $expect_extracted ]		|| err "invalid number of extracted files"
+	[ $(grep "source upgrade files" $LOG.sum |cut -d: -f2) == $expect_sources ]     || err "bad number of source files"
+	[ $(grep "total number of records" $LOG.sum |cut -d: -f2) == $expect_records ]  || err "bad number of records found"
+	[ $(grep "unknown records" $LOG.sum |cut -d: -f2) == $expect_unknown ]          || err "bad number of unknown records"
+	[ $(grep "records use binwalk" $LOG.sum |cut -d: -f2) == $expect_binwalk ]      || err "bad number of binwalk uses"
+	[ $(grep "warnings" $LOG.sum |cut -d: -f2) == 0 ]                               || err "warnings found"
+	[ $(grep "extracted files" $LOG.sum |cut -d: -f2) == $expect_extracted ]        || err "invalid number of extracted files"
 
 	echo test ok
 }
